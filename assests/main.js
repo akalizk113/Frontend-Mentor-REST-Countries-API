6e9b0f1eec8b5b142b55887a72218891d012f9ee
Pham Tuan Anh <akalizk17@gmail.com>
2021-07-10 22:39:43 +0700

    dark-mode-modify

--- assests/main.js
+++ assests/main.js
@@ -1,8 +1,28 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-fetch('https://restcountries.eu/rest/v2/all')
-   .then(response => response.json())
-   .then(data => {
-      console.log(data);
-   })
+const darkModeBtn = $('.btn.dark-mode-btn')
+
+// Function
+const darkmode = () => {
+   darkModeBtn.onclick = () => {
+      $('body').classList.toggle('dark-mode')
+   }
+}
+
+
+
+
+
+const app = () => {
+   darkmode();
+}
+
+app();
+
+
+// fetch('https://restcountries.eu/rest/v2/all')
+//    .then(response => response.json())
+//    .then(data => {
+//       console.log(data);
+//    })
